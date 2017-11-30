28434d4e37ffc44fad1b9cb0770ee0ff9c4f1706
tombeauf <tom.beaufoy@ontrackretail.co.uk>
2017-11-30 17:53:03 +0000

    Adds coinbase dependency and api key / secret

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,22 @@
 import { version } from '../../package.json';
 import { Router } from 'express';
 import facets from './facets';
+import { Client } from 'coinbase';
+import secrets from '../secrets.json';
+
+var client = new Client({'apiKey': secrets.apiKey, 'apiSecret': secrets.apiSecret});
 
 export default ({ config, db }) => {
-	let api = Router();
+    let api = Router();
 
-	// mount the facets resource
-	api.use('/facets', facets({ config, db }));
+    console.log('made it');
+    // mount the facets resource
+    api.use('/facets', facets({ config, db }));
 
-	// perhaps expose some API metadata at the root
-	api.get('/', (req, res) => {
-		res.json({ version });
-	});
+    // perhaps expose some API metadata at the root
+    api.get('/historical-prices', (req, res) => {
+        res.json({ version });
+    });
 
-	return api;
+    return api;
 }
