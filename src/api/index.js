d7611c2245908131b9f0d5e251332da20fc9bb80
beaufoyt <beaufoyyy@live.com>
2017-12-04 23:06:32 +0000

    Refactors a bunch and combines order book logic with rsi data

--- src/api/index.js
+++ src/api/index.js
@@ -1,85 +1,52 @@
 import { version } from '../../package.json';
-import { ATR } from 'technicalindicators';
 import { Router } from 'express';
-import axios from 'axios';
-import moment from 'moment';
+import {
+    closePositionLimit as bitmexClosePositionLimit,
+    closePosition as bitmexClosePosition,
+    openOrder as bitmexOpenOrder,
+    getPosition,
+    setLeverage,
+    getOrderBook,
+    getOrderBookValues
+} from '../clients/bitmexHttp';
+import {
+    startDataLoop as startRangeDataLoop,
+    getRsiRange,
+    getAtrRange
+} from '../clients/gdax';
 
-import facets from './facets';
-import { makeRequest as bitmexHttpRequest } from '../clients/bitmexHttp';
-// import { Client } from 'coinbase';
-// import secrets from '../secrets.json';
-
-let closingPrices = [];
-let atrData = {};
-let hasMidPrice = false;
 let tradeOpen = false;
 let orderId = 0;
 let buyPrice = 0;
-let sellProfitPrice = 0;
-let sellLossPrice = 0;
-let ATRValue = 0;
 let buyRatio = 0;
-let highestBuyRatio = 0;
-let lowestBuyRatio = 100;
 
-const candleSize = 5;
-const atrPeriod = 50;
-const buyPeriod = 21;
-const buyBuyPeriod = 50;
 const bidSize = 1000;
 const orderDepth = 35;
-let median = 0;
 
 let direction = "";
 let trend = "";
 
+setLeverage(100);
+// bitmexClosePosition();
 
-// var client = new Client({'apiKey': secrets.apiKey, 'apiSecret': secrets.apiSecret});
-
-bitmexHttpRequest('post', '/position/leverage', { symbol: 'XBTUSD', leverage: 100 }, (response) => {//
-   console.log(response);
- });
-
-// bitmexHttpRequest('post', '/order', { symbol: 'XBTUSD', orderQty: 100, ordType: 'Market' }, (response) => {
-//     console.log(response);
-// });
-
-// bitmexHttpRequest('post', '/order', { symbol: 'XBTUSD', execInst: 'Close' }, (response) => {
-//     console.log(response);
-// });
-
-// bitmexHttpRequest('post', '/order', { symbol: 'XBTUSD', orderQty: 100, ordType: 'Market' }, (response) => {
-//     console.log(response);
-// });
-
-// bitmexHttpRequest('get', '/order', null, (response) => {
-//     console.log(response.data[response.data.length - 1]);
-// });
-
-const setClosePosition = () => {
+const closePositionLimit = () => {
     var closePrice = direction == 'Buy' ? buyPrice + 20 : buyPrice - 20;
-    bitmexHttpRequest('post', '/order', { symbol: 'XBTUSD', execInst: 'Close', price:  closePrice}, (response) => {
-        console.log('Position close set at :', response.data.price);
-    });
+    bitmexClosePositionLimit(closePrice);
 }
 
 const closePosition = () => {
-    var closePrice = direction == 'Buy' ? buyPrice + 20 : buyPrice - 20;
-    bitmexHttpRequest('post', '/order', { symbol: 'XBTUSD', execInst: 'Close'}, (response) => {
-        console.log('Position closed  at :', response.data.price);
-    });
+    bitmexClosePosition();
 }
 
 const openOrder = (tradeDirection) => {
     direction = tradeDirection;
     if (!tradeOpen) {
-        console.log(direction);
-        bitmexHttpRequest('post', '/order', { symbol: 'XBTUSD', orderQty: bidSize, ordType: 'Market', side: direction}, (response) => {
-
+        console.log(`Direction > ${direction}`);
+        bitmexOpenOrder(bidSize, direction, (response) => {
             orderId = response.data.orderID;
             buyPrice = response.data.price;
             tradeOpen = true;
-            setClosePosition();
+            closePositionLimit();
             console.log('Order opened! ID:', orderId, 'price:', buyPrice);
         });
     }
@@ -87,70 +54,95 @@ const openOrder = (tradeDirection) => {
 
 
 const tradeLogic = () => {
-       
     // get order book data
-    axios.get('https://www.bitmex.com/api/v1/orderBook/L2?symbol=XBT&depth=' + orderDepth).then(response => {
+    const rsi21Indicator = getRsiRange(21)[0];
+    const atrIndicator = getAtrRange()[0];
 
-        var orderBookArray = response.data;
-        const askPrice = orderBookArray[orderDepth-1].price;
-        const bidPrice =  orderBookArray[orderDepth].price;
-        median = (bidPrice + askPrice) / 2
-        console.log('Bid '+ bidPrice + ' Ask ' + askPrice);
-        var buyTotal = 0;
-        var sellTotal = 0;
-        // see if any positions open
-        bitmexHttpRequest('get', '/position',null, (positionResponse) => {
-            // if no postiions open
-            if (positionResponse != null &&  positionResponse.data != null && positionResponse.data[0].currentQty == 0 ){
-                tradeOpen = false;
-                 for (var i = 0; i < orderBookArray.length; i++) {
-                    if (orderBookArray[i].side == "Buy"){
-                        buyTotal = buyTotal + orderBookArray[i].size;
+    if (rsi21Indicator && atrIndicator) {
+        console.log(`RSI 21: ${rsi21Indicator} ATR: ${atrIndicator}`);
+
+        getOrderBook(orderDepth, (response) => {
+
+            var orderBookArray = response.data;
+
+            const { askPrice, bidPrice, median } = getOrderBookValues(orderBookArray, orderDepth);
+            console.log('Bid '+ bidPrice + ' Ask ' + askPrice);
+            var buyTotal = 0;
+            var sellTotal = 0;
+            // see if any positions open
+            getPosition((positionResponse) => {
+                // if no postiions open
+                if (positionResponse != null &&  positionResponse.data != null && positionResponse.data[0].currentQty == 0 ){
+                    tradeOpen = false;
+                     for (var i = 0; i < orderBookArray.length; i++) {
+                        if (orderBookArray[i].side == "Buy"){
+                            buyTotal = buyTotal + orderBookArray[i].size;
+                        }
+                        if (orderBookArray[i].side == "Sell"){
+                            sellTotal = sellTotal + orderBookArray[i].size;
+                        }
                     }
-                    if (orderBookArray[i].side == "Sell"){
-                        sellTotal = sellTotal + orderBookArray[i].size;
-                    } 
-                }   
-                buyRatio = Math.round((buyTotal/sellTotal) * 100);
-                if (buyRatio > 500 ){ //&& trend == "UP"
-                    openOrder('Buy');          
+                    buyRatio = Math.round((buyTotal/sellTotal) * 100);
+                    if (buyRatio > 500){ //&& trend == "UP"
+                        openOrder('Buy');
+                    }
+                    if (buyRatio < 20 && trend == "DOWN"){
+                        openOrder('Sell');
+                    }
+                    console.log('Current Price:', median,  ' Buy percentage:', buyRatio );
+                    console.log('==========================================================')
+                } else {
+                    tradeOpen = true;
+                    if (positionResponse.data[0].currentQty > 0) {
+                        direction = 'Buy';
+                    }
+                    if (positionResponse.data[0].currentQty < 0) {
+                        direction = 'Sell';
+                    }
+                    var dollarMovement =  direction == 'Buy' ? median - buyPrice :  buyPrice - median;
+                    console.log(direction, ' trade open at:', buyPrice, ' Current Price:', median, ' Making:', dollarMovement );
+                    console.log('==========================================================')
+                    if (dollarMovement < -20 || dollarMovement  > 20) {
+                        closePosition();
+                    }
                 }
-                if (buyRatio < 20 && trend == "DOWN"){
-                    openOrder('Sell');
-                }
-                console.log('Current Price:', median,  ' Buy percentage:', buyRatio );
-                console.log('==========================================================')
-            } else {
-                tradeOpen = true;
-                if (positionResponse.data[0].currentQty > 0) {
-                    direction = 'Buy';
-                }
-                if (positionResponse.data[0].currentQty < 0) {
-                    direction = 'Sell';
-                }
-                var dollarMovement =  direction == 'Buy' ? median - buyPrice :  buyPrice - median;
-                console.log(direction , ' trade open at : ', buyPrice, ' Current Price:', median, 'Making:', dollarMovement );
-                console.log('==========================================================')
-                if (dollarMovement < -20 || dollarMovement  > 20) {
-                    closePosition();
-                }   
-            }   
-        });
-    }, err => console.log(err));
+            });
+        }, err => console.log(err));
+    }
 };
 
+// Params: candleSize, numCandles, atrPeriod, interval in seconds
+startRangeDataLoop(5, 110, 50, 2);
 
 setInterval( tradeLogic, 2 * 1000);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // API stuff for when we need it.
-export default ({ config, db }) => {
+export default (/*{ config, db }*/) => {
     let api = Router();
 
-    // mount the facets resource
-    api.use('/facets', facets({ config, db }));
-
-    // perhaps expose some API metadata at the root
     api.get('/version', (req, res) => {
         res.send(version)
     });
